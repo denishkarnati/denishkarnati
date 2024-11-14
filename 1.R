e79eac1d3d9520aa7333f2164fde00d8091852b3
denishkarnati <167035554+denishkarnati@users.noreply.github.com>
2024-11-14 13:33:43 -0600

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,20 @@
+#Dinesh Karnati 
+#GR019909
+#Week-2 
+
+# Scatter plot with different shapes for classes
+##The below code initializes ggplot using diabetesTib dataframe, where glucose on x-axis and insulin on y axis and display different shapes based on class variable geom_point to add points & theme_bw to add black and white theme for better visibility
+
+library(mlr)           # Load the 'mlr' package for machine learning tasks.
+library(tidyverse)     # Load the 'tidyverse' package for data manipulation and visualization.
+
+ggplot(diabetesTib, aes(glucose, insulin, 
+                        shape = class)) + 
+  geom_point()  +
+  theme_bw()
+
+# The code below gives different shapes and colors for classes
+ggplot(diabetesTib, aes(glucose, insulin, 
+                        shape = class, col = class)) + 
+  geom_point()  +
+  theme_bw()
